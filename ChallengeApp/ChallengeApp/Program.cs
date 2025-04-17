1e683a3ed1f07ce4568a6b86d221234542929fae
Hancyk Hanc <hancyk7@gmail.com>
2025-04-17 21:23:36 +0200

    dzien4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var sex = "K";
+var age = 20;
+
+if (sex == "K")
+{
+    if (age < 30)
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    else if (age == 30 && name == "Ewa")
+        Console.WriteLine("Ewa lat 30");
+    else
+        Console.WriteLine("");
+
+}
+else if (age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("");
+}
